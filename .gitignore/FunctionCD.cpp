0e79e1fc6a3d39de464b9cf9c96020499ce959ca
NmLa0s <40865715+NmLa0s@users.noreply.github.com>
2018-07-05 19:25:48 +0200

    Create FunctionCD.cpp

---
+++ .gitignore/FunctionCD.cpp
@@ -0,0 +1,30 @@
+#include "CDfunctin"
+#include "runtimezero"
+#include "fsus"
+#include <cstream>
+
+void (FSTR size, fOut file, DYNAMIC dyn)
+
+main()
+{
+// still not add 
+
+}
+
+
+// ... con function of CD
+void (FSTR size, fOut file, DYNAMIC dyn)
+{
+ size[] = {C, D, , B , A , F}
+ dout.comparison = CDpath("//used exe // used dll "); // used any 
+ file = dout.comparison(1);
+ loop(0);
+ T3HELPER startip;
+ T3helperP startup;
+ if(dout.comparison = dout.startip || dout.comparison = dout.startup)
+ {
+ fillpath("debug");
+ makenew("CD/desktop");
+ // cont  . . . .
+ }
+}
