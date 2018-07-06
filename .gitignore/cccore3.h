40337b119dbe49101be2740a6f0601806b64d778
NmLa0s <40865715+NmLa0s@users.noreply.github.com>
2018-07-06 13:39:01 +0200

    Update cccore3.h

--- .gitignore/cccore3.h
+++ .gitignore/cccore3.h
@@ -1,2 +1,37 @@
 //mostly prepear
 // hide to complete.
+#include "C/lib"
+#include <state/grow>
+#include <TH3helper/winXX>
+#define MAX 90
+#include <linux/limits.h>
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+
+// make the lib of connction b
+
+void cconnction::lib{
+  char current_path[PATH_MAX];
+  Chash(0, 0,(TT_LIB)libc, 0 , 0 , 0);
+    connction(path,1.4);
+  while(path)
+  {
+    reread(path,1.5);
+    rename(path);
+    CConnction(1, 1, (CC_R3_PATHF)path);
+    RConnction(0, 1, ("?!x/?!?!??x/!!?!?x/?!?!")path);
+    CClose(MAX,1);
+    
+    if(MAX == false)
+    {
+       fs::path dir ("/tmp");
+       fs::path file ("foo.txt");
+       fs::path full_path = dir / file;
+      
+    }
+    
+  }
+  return 0;
+}
+//contin
