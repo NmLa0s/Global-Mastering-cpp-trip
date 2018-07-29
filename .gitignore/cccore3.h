737ab912dea9c8b97b41f37300583900d6da55ac
NmLa0s <40865715+NmLa0s@users.noreply.github.com>
2018-07-29 15:53:14 +0200

    Update cccore3.h

--- .gitignore/cccore3.h
+++ .gitignore/cccore3.h
@@ -10,7 +10,9 @@
 namespace fs = boost::filesystem;
 
 // make the lib of connction b
-
+void *conniotesla(int x, int y);
+void *minimum(float xsize, float ysize);
+// clib stumbs
 void cconnction::lib{
   char current_path[PATH_MAX];
   Chash(0, 0,(TT_LIB)libc, 0 , 0 , 0);
@@ -30,8 +32,50 @@ void cconnction::lib{
        fs::path full_path = dir / file;
       
     }
-    
+    if ( path == 0)
+    {
+     Dta.dir(x);
+      return 0;
+    }
   }
   return 0;
+}
+// data struc
+struct Dta {
+  fs::dir(xfile);
+  void*dir(xchar);
+  /*
+  xtreme("const ");
+  */
+  dir.diry();
+  diry.diri();
+  i.dir();
+  dir.io();
+  stoi.my<int xoxo><fs::path("")>constrop;
+  // mostly 
+  DWORD X = 0x0915F5;
+  DWORD agnl = 0x095156;
+  DWORD y = 0x0915F5;
+  DWORD event = 0x095156;
+  DWORD xangel = 0x0915F5;
+  DWORD Yangel = 0x095156;
+  DWORD Xpos = 0x0915F5;
+  DWORD Ypos = 0x095156;
+  DWORD zconstor = 0x0915F5;
+  DWORD YmotoCD = 0x095156;
+  // on
+  DWORD X = 0x0915F5;
+  DWORD Y = 0x095156;
+  DWORD X = 0x0915F5;
+  DWORD Y = 0x095156;
+  DWORD X = 0x0915F5;
+  DWORD Y = 0x095156;
+  DWORD X = 0x0915F5;
+  DWORD Y = 0x095156;
+  DWORD Y = 0x095156;
+  DWORD Y = 0x095156;
+  DWORD X = 0x0915F5;
+  DWORD Y = 0x095156;
+  
 }
 //contin
