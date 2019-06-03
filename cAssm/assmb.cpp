7d4380aae48956c33c81e9474ab99ec65f7929fc
fdxb1 <40865715+NmLa0s@users.noreply.github.com>
2019-06-03 21:17:10 +0200

    Update assmb.cpp

--- cAssm/assmb.cpp
+++ cAssm/assmb.cpp
@@ -1,8 +1,34 @@
+#include <iostream>
+#include <string>
+#define TRUE = 1
+#define TABLE_NUM 16
+#define NEW 0
+#define COMATAPLE 0
+
 class NDTEvent {
 public:
+	int digits_i;
+	int sI_i;
+	int sII_i;
+	int sbase_i;
+
 	NDTEvent(){
 		_trueEvent = 1;
 		_EventTaple = TABLE_NUM;
+		
+	std::cout <<"[Service] - Please put the owner digits: ";
+	std::cin >> digits_i;
+	InputDigits(digits_i);
+	if(digits_i){
+		std::cout <<"[Service check] - done"<<std::endl;}
+
+	std::cout <<"[Service] - Please Input the 3 dim digits into the system in way [ num1 , num2, num3 ]: ";
+	std::cin >> sbase_i >> sI_i >> sII_i;
+	CalcSincs(sbase_i, sI_i, sII_i);
+
+	if( _active_b = 1 , _active_b = 2 ){
+
+		std::cout <<"[Service check] - done"<<std::endl;}
 	}
 
 	void InputDigits( int digit ){ digit = _digit; };
@@ -21,6 +47,7 @@ public:
 			sI += 50;
 			sII+= 50;
 			std::cout << "sI & SII have been completed from the system" << std::endl;
+			_active_b = 1;
 		}
 		else{
 			since_base = sI / sII;
@@ -28,7 +55,10 @@ public:
 			_EventTaple = COMATAPLE;
 
 			std::cout << " Our system is ready to generate an key for this ID:" << _digit + since_base << std::endl;
+			_active_b = 2;
 		}
+
+		
 	};
 
 private:
@@ -40,6 +70,7 @@ private:
 	int _szFwD; // not used
 
 	bool _b; // not used
+	bool _active_b;
 	float _f; // not used
 
 protected:
@@ -48,24 +79,7 @@ protected:
 };
 
 int main(){
-	NDTEvent Event;
-	NDTEvent Event_handle();
-	int digits_i;
-	int sI_i;
-	int sII_i;
-	int sbase_i;
-
-	std::cout <<"[Service] - Please put the owner digits: ";
-	std::cin >> digits_i;
-	Event.InputDigits(digits_i);
-	
-	std::cout <<"[Service check] - done"<<std::endl;
-	std::cout <<"[Service] - Please Input the 3 dim digits into the system in way [ num1 , num2, num3 ]: ";
-
-	std::cin >> sbase_i >> sI_i >> sII_i;
-	Event.CalcSincs(sbase_i, sI_i, sII_i);
-
-	std::cout <<"[Service check] - done"<<std::endl;
+	NDTEvent Event();
 	
 	system("puase");
 }
